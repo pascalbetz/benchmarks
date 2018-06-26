2f8b75a4caa6095776d1917f6795869bcf9f64b2
Pascal Betz <pascal.betz@swisscom.com>
2018-06-26 09:23:26 +0200

    Add results

--- frozen_string_concat.rb
+++ frozen_string_concat.rb
@@ -42,3 +42,23 @@ Benchmark.ips do |x|
     end
   end
 end
+
+__END__
+Sample run with Ruby 2.5.0
+
+Warming up --------------------------------------
+ array without check     6.305k i/100ms
+    array with check     6.601k i/100ms
+                 dup     8.215k i/100ms
+                  +=     1.899k i/100ms
+Calculating -------------------------------------
+ array without check     68.299k (± 5.9%) i/s -    340.470k in   5.002035s
+    array with check     71.571k (± 5.3%) i/s -    363.055k in   5.087321s
+                 dup     85.958k (± 5.4%) i/s -    435.395k in   5.079625s
+                  +=     18.714k (±25.7%) i/s -     87.354k in   5.039241s
+
+Comparison:
+                 dup:    85958.4 i/s
+    array with check:    71571.4 i/s - 1.20x  slower
+ array without check:    68299.1 i/s - 1.26x  slower
+                  +=:    18713.8 i/s - 4.59x  slower
--- method_vs_public_send_vs_send.rb
+++ method_vs_public_send_vs_send.rb
@@ -24,3 +24,19 @@ Benchmark.ips do |x|
     obj.send(:hello)
   end
 end
+
+__DATA__
+Sample Run with Ruby 2.5.0
+Warming up --------------------------------------
+       method + call   157.777k i/100ms
+         public_send   246.986k i/100ms
+                send   267.647k i/100ms
+Calculating -------------------------------------
+       method + call      2.571M (± 2.7%) i/s -     12.938M in   5.035042s
+         public_send      6.042M (± 3.3%) i/s -     30.379M in   5.033482s
+                send      7.656M (± 4.0%) i/s -     38.274M in   5.007630s
+
+Comparison:
+                send:  7655632.3 i/s
+         public_send:  6041856.5 i/s - 1.27x  slower
+       method + call:  2571436.8 i/s - 2.98x  slower
